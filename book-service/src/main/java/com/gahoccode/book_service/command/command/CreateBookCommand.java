7e107c3930b08e25dfe2350a047e95af10ab799d
dominhit215@gmail.com <dominhit215@gmail.com>
2025-05-05 06:56:29 +0700

    add post command book

---
+++ book-service/src/main/java/com/gahoccode/book_service/command/command/CreateBookCommand.java
@@ -0,0 +1,23 @@
+package com.gahoccode.book_service.command.command;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateBookCommand {
+
+    @TargetAggregateIdentifier
+    private String bookId;
+
+    private String name;
+
+    private String author;
+
+    private Boolean isReady;
+}
---
+++ book-service/src/main/java/com/gahoccode/book_service/command/controller/BookCommandController.java
@@ -0,0 +1,26 @@
+package com.gahoccode.book_service.command.controller;
+
+import com.gahoccode.book_service.command.command.CreateBookCommand;
+import com.gahoccode.book_service.command.model.BookRequestModel;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/books")
+public class BookCommandController {
+
+    @Autowired
+    private CommandGateway commandGateway;
+
+    @PostMapping
+    public String addBook(@RequestBody BookRequestModel model){
+        CreateBookCommand command = new CreateBookCommand(UUID.randomUUID().toString(), model.getName(), model.getAuthor(), true);
+        return commandGateway.sendAndWait(command);
+    }
+}
