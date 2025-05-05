e0fd600783c6f68b42a5e229b04d4213ef0a2fea
dominhit215@gmail.com <dominhit215@gmail.com>
2025-05-05 17:58:46 +0700

    validation request body

--- book-service/src/main/java/com/gahoccode/book_service/command/controller/BookCommandController.java
+++ book-service/src/main/java/com/gahoccode/book_service/command/controller/BookCommandController.java
@@ -4,6 +4,7 @@ import com.gahoccode.book_service.command.command.CreateBookCommand;
 import com.gahoccode.book_service.command.command.DeleteBookCommand;
 import com.gahoccode.book_service.command.command.UpdateBookCommand;
 import com.gahoccode.book_service.command.model.BookRequestModel;
+import jakarta.validation.Valid;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -18,7 +19,7 @@ public class BookCommandController {
     private CommandGateway commandGateway;
 
     @PostMapping
-    public String addBook(@RequestBody BookRequestModel model){
+    public String addBook(@Valid @RequestBody BookRequestModel model){
         CreateBookCommand command = new CreateBookCommand(UUID.randomUUID().toString(),model.getName(),model.getAuthor(),true);
         return commandGateway.sendAndWait(command);
     }
