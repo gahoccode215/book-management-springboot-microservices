49da69bd0c2470adb9b159ec70cc3884ae462a51
dominhit215@gmail.com <dominhit215@gmail.com>
2025-05-08 01:19:24 +0700

    add api filter employee

---
+++ employee-service/src/main/java/com/gahoccode/employee_service/query/queries/GetDetailEmployeeQuery.java
@@ -0,0 +1,12 @@
+package com.gahoccode.employee_service.query.queries;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetDetailEmployeeQuery {
+    private String id;
+}
