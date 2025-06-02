a6e66f337bcc4dcc9e0923bc27710409b6c16e00
dominhit215@gmail.com <dominhit215@gmail.com>
2025-06-03 01:05:00 +0700

    update

---
+++ shopping-app/product-service/src/main/java/com/gahoccode/product/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.gahoccode.product.repository;
+
+import com.gahoccode.product.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepository extends MongoRepository<Product, String> {
+}
